070f547f2118b3fd762bf733f7c6ed4068a450ac
agramonte <me@adriangomez.com>
2018-01-11 00:09:10 -0500

    Rewarded Ad & Event to match Admob
    Rewarded ads have been implemented but not tested.
    Changed all the events to match Admob events (to the best of my
    knowledge).

--- Corona/main.lua
+++ Corona/main.lua
@@ -5,27 +5,30 @@ local function somoListerner( event )
 	print("++++smaato Event:", json.prettify(event))
 	
 	if event.phase == "init" then
-		
-        smaato.show( "banner", {adSpaceId = 0, y="bottom", autoRefresh=true, refreshInterval=45 })
+    smaato.show( "banner", {adSpaceId = 0, y="bottom", autoRefresh=true, refreshInterval=45 })
 		smaato.load( "interstitial", {adSpaceId = 0})
-		
-    elseif event.phase == "loaded" then
-        if event.isError == true then
-            
-            btimer.performWithDelay( 45000, function()
-                smaato.load( "interstitial", {adSpaceId = 0}) 
-			end )
-			
-        end
-	elseif event.phase == "hidden" then
+    smaato.load( "rewardedVideo", {adSpaceId = 0})
+	
+  elseif event.phase == "hidden" then
 		smaato.load( "interstitial", {adSpaceId = 0} ) 
-		
+		smaato.show( "banner", {adSpaceId = 0, y="top", autoRefresh=true, refreshInterval=45 })
     end
 end
 
-smaato.init( somoListerner, { pubId = 0} )
+smaato.init( somoListerner,{ pubId = 0} )
             
 timer.performWithDelay( 30000, function()
-	smaato.show("interstitial")
+	if smaato.isLoaded( "interstitial" ) then
+		smaato.hide("banner")
+		smaato.show("interstitial")
+	end
 end, 1 )
+
+timer.performWithDelay(45000, function()
+  if smaato.isLoaded( "rewardedVideo" ) then
+    smaato.hide("banner")
+    smaato.show("rewardedVideo")
+  end
+end, 1)
+
 
